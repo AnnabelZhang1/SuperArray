045d809a153c23d9be6f4488952c3b808105738d
AnnabelZhang1 <azhang22@stuy.edu>
2020-11-03 12:44:28 -0500

    added SuperArray

---
+++ SuperArray.java
@@ -0,0 +1,33 @@
+public class SuperArray{
+
+  private String[] data;
+  private int size;
+
+  //constructor
+  public SuperArray()
+    {
+      String[] superArray = new String[10];
+    }
+
+  //methods
+  public int size()
+    {
+      return
+    }
+
+  public boolean add(String element)
+    {
+
+    }
+
+  public String set(int index, String element)
+    {
+
+    }
+
+  private void resize()
+    {
+      
+    }
+
+}
