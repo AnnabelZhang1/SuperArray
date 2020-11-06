7acf9ed950a0af2253d6e5a83999c106e4727dad
AnnabelZhang1 <azhang22@stuy.edu>
2020-11-06 12:13:14 -0500

    added removeDuplicates

--- Demo.java
+++ Demo.java
@@ -1,4 +1,6 @@
 public class Demo{
+
+  //index of to find first occurrence of word; remove to get rid of else
   public static void removeDuplicates(SuperArray s)
     {
       for (int i = 0; i < s.size(); i++)
@@ -11,14 +13,22 @@ public class Demo{
               i -= 1;
             }
         }
-        //index of to find first occurrence of word; remove to get rid of else
     }
-
+/*
+  public static SuperArray findOverlap(SuperArray a, SuperArray b)
+    {
+      Superarray[] overlap = new SuperArray();
+      for (int i = 0; i < a.size(); i++)
+        {
+          if
+        }
+      return overlap;
+    }
+*/
   public static void main(String[] args)
     {
       SuperArray words = new SuperArray();
 
-      //grouped to save vertical space
       words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
       words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
       words.add("una");    words.add("ebi");     words.add("toro");
@@ -26,10 +36,14 @@ public class Demo{
       System.out.println(words);
       removeDuplicates(words);
       System.out.println(words);
+
+      /*
+      expected:
+      [kani, uni, ebi, una, una, ebi, kani, una, una, ebi, toro]
+      [kani, uni, ebi, una, toro]
+      */
+
+//      System.out.println(findOverlap( ["9","1","2","2","3","4"], ["0","4","2","2","9"] ));
+      //expected ["9","2","4"]
     }
 }
-
-/*expected
-[kani, uni, ebi, una, una, ebi, kani, una, una, ebi, toro]
-[kani, uni, ebi, una, toro]
-*/
